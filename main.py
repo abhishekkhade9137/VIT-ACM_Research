af58756767523e7e0745053d0b0656e564598009
abhishekkhade9137 <164767347+abhishekkhade9137@users.noreply.github.com>
2024-03-24 20:56:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras.layers import *
+from keras.models import *
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+import os, shutil
+import warnings
+warnings.filterwarnings('ignore')
+
+train_path = "../input/newnew1/upload/train"
+validation_path = "../input/newnew1/upload/validation"
+test_path = "../input/newnew1/upload/test"
+
+image_categories = os.listdir('../input/newnew1/upload/train')
+
+train_gen = ImageDataGenerator(rescale = 1.0/255.0) # Normalise the data
+train_image_generator = train_gen.flow_from_directory(
+                                            train_path,
+                                            target_size=(150, 150),
+                                            batch_size=32,
+                                            class_mode='categorical')
+
+val_gen = ImageDataGenerator(rescale = 1.0/255.0) # Normalise the data
+val_image_generator = train_gen.flow_from_directory(
+                                            validation_path,
+                                            target_size=(150, 150),
+                                            batch_size=32,
+                                            class_mode='categorical')
+
+test_gen = ImageDataGenerator(rescale = 1.0/255.0) # Normalise the data
+test_image_generator = train_gen.flow_from_directory(
+                                            test_path,
+                                            target_size=(150, 150),
+                                            batch_size=32,
+                                            class_mode='categorical')
+
+class_map = dict([(v, k) for k, v in train_image_generator.class_indices.items()])
+print(class_map)
+
+model = Sequential()
+
+
+model.add(Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu', input_shape=[150, 150, 3]))
+model.add(MaxPooling2D(2, ))
+model.add(Conv2D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu'))
+model.add(MaxPooling2D(2))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation='relu'))
+#model.add(Dense(128, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+model.summary()
+
+
+early_stopping = keras.callbacks.EarlyStopping(patience=5000) # Set up callbacks
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics='accuracy')
+hist = model.fit(train_image_generator, 
+                 epochs=20, 
+                 verbose=1, 
+                 validation_data=val_image_generator, 
+                 steps_per_epoch = 394//32, 
+                 validation_steps = 126//32)
+
+
+h = hist.history
+plt.style.use('ggplot')
+plt.figure(figsize=(10, 5))
+plt.plot(h['loss'], c='red', label='Training Loss')
+plt.plot(h['val_loss'], c='red', linestyle='--', label='Validation Loss')
+plt.plot(h['accuracy'], c='blue', label='Training Accuracy')
+plt.plot(h['val_accuracy'], c='blue', linestyle='--', label='Validation Accuracy')
+plt.xlabel("Number of Epochs")
+plt.legend(loc='best')
+plt.show()
+
+
+
+
+model.evaluate(test_image_generator)
+
+
+
+
+test_image_path = '../input/newnew1/upload/test/pepe/F7SYtltW8AAldye.jpeg'
+def generate_predictions(test_image_path, actual_label):
+    
+    test_img = image.load_img(test_image_path, target_size=(150, 150))
+    test_img_arr = image.img_to_array(test_img)/255.0
+    test_img_input = test_img_arr.reshape((1, test_img_arr.shape[0], test_img_arr.shape[1], test_img_arr.shape[2]))
+
+    predicted_label = np.argmax(model.predict(test_img_input))
+    predicted_vegetable = class_map[predicted_label]
+    plt.figure(figsize=(4, 4))
+    plt.imshow(test_img_arr)
+    plt.title("Predicted Label: {}, Actual Label: {}".format(predicted_vegetable, actual_label))
+    plt.grid()
+    plt.axis('off')
+    plt.show()
+
+
+generate_predictions(test_image_path, actual_label='pepe')
+
